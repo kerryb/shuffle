2dca216bca5907714ba7c9701a72d1831633f13b
Kerry Buckley <kerryjbuckley@gmail.com>
2025-09-28 08:56:49 +0100

    Only generate solution inputs when enumeration is changed and valid

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-document.getElementById("enumeration").addEventListener("input", enumerationChanged)
+document.getElementById("enumeration").addEventListener("change", enumerationChanged)
 
 function enumerationChanged(e) {
-  const solution = document.getElementById("solution")
-  solution.innerHTML = ""
+  e.target.reportValidity()
+  if (e.target.validity.valid) {
+    const solution = document.getElementById("solution")
+    solution.innerHTML = ""
 
-  const iterator = e.target.value[Symbol.iterator]()
-  let char = iterator.next()
-  while (!char.done) {
-    if (char.value == "/") {
-      solution.append(" ")
-    } else if (char.value == "/" || char.value == "-") {
-      solution.append("-")
-    } else {
-      for (let n = 0; n < parseInt(char.value); n++) {
-      const element = document.createElement("input")
+    const iterator = e.target.value[Symbol.iterator]()
+    let char = iterator.next()
+    while (!char.done) {
+      if (char.value == "/") {
+        solution.append(" ")
+      } else if (char.value == "/" || char.value == "-") {
+        solution.append("-")
+      } else {
+        for (let n = 0; n < parseInt(char.value); n++) {
+          const element = document.createElement("input")
 
-        const maxlength = document.createAttribute("maxlength")
-        maxlength.value = "1"
-        element.setAttributeNode(maxlength)
+          const maxlength = document.createAttribute("maxlength")
+          maxlength.value = "1"
+          element.setAttributeNode(maxlength)
 
-      const size = document.createAttribute("size")
-      size.value = "1"
-      element.setAttributeNode(size)
-      solution.appendChild(element)
+          const size = document.createAttribute("size")
+          size.value = "1"
+          element.setAttributeNode(size)
+          solution.appendChild(element)
+        }
       }
+      char = iterator.next()
     }
-    char = iterator.next()
   }
 }
