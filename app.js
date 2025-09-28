f6f3550a10abd6b74ec639949b8dd7af677b9729
Kerry Buckley <kerryjbuckley@gmail.com>
2025-09-28 09:15:27 +0100

    Extract functions

--- app.js
+++ app.js
@@ -3,29 +3,37 @@ document.getElementById("enumeration").addEventListener("change", enumerationCha
 function enumerationChanged(e) {
   e.target.reportValidity()
   if (e.target.validity.valid) {
-    const solution = document.getElementById("solution")
-    solution.innerHTML = ""
+    addSolutionInputs(e.target.value)
+  }
+}
 
-    for (const value of e.target.value.split(/([,-])/)) {
-      console.log(value)
-      if (value == ",") {
-        solution.append(" ")
-      } else if (value == "-") {
-        solution.append("-")
-      } else {
-        for (let n = 0; n < parseInt(value); n++) {
-          const element = document.createElement("input")
+function addSolutionInputs(enumeration) {
+  const solution = document.getElementById("solution")
+  solution.innerHTML = ""
 
-          const maxlength = document.createAttribute("maxlength")
-          maxlength.value = "1"
-          element.setAttributeNode(maxlength)
-
-          const size = document.createAttribute("size")
-          size.value = "1"
-          element.setAttributeNode(size)
-          solution.appendChild(element)
-        }
-      }
+  for (const value of enumeration.split(/([,-])/)) {
+    console.log(value)
+    if (value == ",") {
+      solution.append(" ")
+    } else if (value == "-") {
+      solution.append("-")
+    } else {
+      addSolutionWord(parseInt(value))
     }
+  }
+}
+
+function addSolutionWord(letterCount) {
+  for (let n = 0; n < letterCount; n++) {
+    const element = document.createElement("input")
+
+    const maxlength = document.createAttribute("maxlength")
+    maxlength.value = "1"
+    element.setAttributeNode(maxlength)
+
+    const size = document.createAttribute("size")
+    size.value = "1"
+    element.setAttributeNode(size)
+    solution.appendChild(element)
   }
 }
