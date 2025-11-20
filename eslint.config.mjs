35080a14d88e19eb4236434dd38cf039e57b4e41
Kerry Buckley <kerryjbuckley@gmail.com>
2025-11-20 17:21:49 +0000

    Add CSS linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,16 @@
 import js from "@eslint/js"
 import globals from "globals"
 import { defineConfig } from "eslint/config"
+import css from "@eslint/css"
 
 export default defineConfig([
+  {
+		files: ["**/*.css"],
+    ignores: ["app/reset.css"],
+		language: "css/css",
+		plugins: { css },
+		extends: ["css/recommended"],
+	},
   {
     files: ["**/*.{js,mjs,cjs}"],
     ignores: ["**/*.config.*js"],
